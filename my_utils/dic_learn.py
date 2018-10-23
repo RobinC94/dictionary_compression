f9c7536d5d19b3b3ce0e7628d7e626e4fb4e6705
leonharddt <sissure@qq.com>
2018-10-23 20:38:06 +0800

    3x3 dict complete

--- my_utils/dic_learn.py
+++ my_utils/dic_learn.py
@@ -4,19 +4,20 @@ from sklearn.decomposition import MiniBatchDictionaryLearning
 fit_algorithm = 'cd'
 transform_algorithm = 'omp'
 transform_n_nonzero_coefs = 2
-n_iter=50
+n_iter=100
 
 def comp_kernel(kernel, n_components=16):
     num = np.shape(kernel)[3]
     channel = np.shape(kernel)[2]
     filter_size = np.shape(kernel)[:2]
-    dic_list = []
-    index_list = []
-    a_list = []
-    b_list = []
-    avg_e = 0
     print np.shape(kernel)
 
+    k = kernel
+    k.shape = (filter_size[0]*filter_size[1]*channel, num)
+    k=k.transpose()
+    v, index, a1, a2, e = dict_learn(k, n_components=n_components)
+
+    '''
     for i in range(channel):
         k = kernel[:,:,i,:]
         k.shape = (filter_size[0]*filter_size[1], num)
@@ -28,10 +29,11 @@ def comp_kernel(kernel, n_components=16):
         a_list.append(a1)
         b_list.append(a2)
         avg_e += e
+    '''
 
-    print 'num: ', num, 'comp: ', n_components, 'avg error: ', avg_e/channel
+    print 'num: ', num, 'comp: ', n_components, 'error: ', e
 
-    return dic_list, index_list, a_list, b_list
+    return v, index, a1, a2, e
 
 def dict_learn(y, n_components=16, n_iter=n_iter):
     n = np.shape(y)[0]
@@ -46,25 +48,41 @@ def dict_learn(y, n_components=16, n_iter=n_iter):
     index = []
     a1=[]
     a2=[]
-    nz = np.nonzero(x)[1]
+    nz = np.nonzero(x)
+    #print(np.nonzero(x))
+    #print(np.where(nz[0] == 1))
+    #print(len(nz), n*2)
     for i in range(n):
-        l=[nz[i*2], nz[i*2+1]]
-        index.append(l)
-        a1.append(x[i,l[0]])
-        a2.append(x[i,l[1]])
-    res = np.dot(x,v)
-    error = np.mean(np.linalg.norm(y-res, axis=1))
+        nz_i = np.where(nz[0] == i)[0]
+        if nz_i.shape[0] == 2:
+            l=[nz[1][nz_i[0]], nz[1][nz_i[1]]]
+            index.append(l)
+            a1.append(x[i,l[0]])
+            a2.append(x[i,l[1]])
+        else:
+            if nz[1][nz_i[0]] == 0:
+                l=[nz[1][nz_i[0]],1]
+            else:
+                l = [nz[1][nz_i[0]], 0]
+            index.append(l)
+            a1.append(x[i, l[0]])
+            a2.append(0)
+
+    #res = np.dot(x,v)
+    #error = np.mean(np.linalg.norm(y-res, axis=1))
+    error = 0
     #print(res)
     #print(error)
     return v, index, a1, a2, error
 
 if __name__ == '__main__':
-    inin = range(3*3*3*32)
+    inin = range(3*3*16*32)
     kernel = np.array(inin)
-    kernel.shape = (3,3,3,32)
-    dic_list, index_list, a_list, b_list = comp_kernel(kernel, n_components=8)
+    kernel.shape = (3,3,16,32)
+    dic, index, a, b, error = comp_kernel(kernel, n_components=8)
 
-    print(np.shape(dic_list))
-    print(np.shape(index_list))
-    print(np.shape(a_list))
-    print(np.shape(b_list))
+    print(np.shape(dic))
+    print(np.shape(index))
+    print(np.shape(a))
+    print(np.shape(b))
+    print(error)
