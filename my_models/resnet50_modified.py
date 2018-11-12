c7b89b8f8c1bf530cb8d1072b8cebb9c5d4354c6
leonharddt <sissure@qq.com>
2018-11-12 21:27:43 +0800

    add lite 11.12

--- my_models/resnet50_modified.py
+++ my_models/resnet50_modified.py
@@ -157,6 +157,141 @@ def ModifiedResNet50(include_top=True,
     return model
 
 
+def identity_block_lite(input_tensor, kernel_size, filters, stage, block, rate=4):
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    import my_models
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(filters2/rate)), kernel_size,
+               padding='same', name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    x = layers.add([x, input_tensor])
+    x = Activation('relu')(x)
+    return x
+
+
+def conv_block_lite(input_tensor, kernel_size, filters, stage, block, strides=(2, 2), rate=4):
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), strides=strides,
+               name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    import my_models
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(filters2/rate)), kernel_size, padding='same',
+               name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = Conv2D(filters3, (1, 1), strides=strides,
+                      name=conv_name_base + '1')(input_tensor)
+    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = layers.add([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+
+def ResNet50_lite(include_top=True,
+             input_tensor=None, input_shape=None,
+             pooling=None,
+             classes=1000,
+                     rate=4):
+
+    # Determine proper input shape
+    input_shape = _obtain_input_shape(input_shape,
+                                      weights='imagenet',
+                                      default_size=224,
+                                      min_size=197,
+                                      data_format=K.image_data_format(),
+                                      require_flatten=include_top)
+
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    import my_models
+
+    x = Conv2D(
+        max(my_models.LEAST_ATOMS, int(64/rate)), (7, 7), strides=(2, 2), padding='same', name='conv1', comp_rate=rate, dict_index=index_gen.next())(img_input)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv_block_lite(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1), rate=rate)
+    x = identity_block_lite(x, 3, [64, 64, 256], stage=2, block='b', rate=rate)
+    x = identity_block_lite(x, 3, [64, 64, 256], stage=2, block='c', rate=rate)
+
+    x = conv_block_lite(x, 3, [128, 128, 512], stage=3, block='a', rate=rate)
+    x = identity_block_lite(x, 3, [128, 128, 512], stage=3, block='b', rate=rate)
+    x = identity_block_lite(x, 3, [128, 128, 512], stage=3, block='c', rate=rate)
+    x = identity_block_lite(x, 3, [128, 128, 512], stage=3, block='d', rate=rate)
+
+    x = conv_block_lite(x, 3, [256, 256, 1024], stage=4, block='a', rate=rate)
+    x = identity_block_lite(x, 3, [256, 256, 1024], stage=4, block='b', rate=rate)
+    x = identity_block_lite(x, 3, [256, 256, 1024], stage=4, block='c', rate=rate)
+    x = identity_block_lite(x, 3, [256, 256, 1024], stage=4, block='d', rate=rate)
+    x = identity_block_lite(x, 3, [256, 256, 1024], stage=4, block='e', rate=rate)
+    x = identity_block_lite(x, 3, [256, 256, 1024], stage=4, block='f', rate=rate)
+
+    x = conv_block_lite(x, 3, [512, 512, 2048], stage=5, block='a', rate=rate)
+    x = identity_block_lite(x, 3, [512, 512, 2048], stage=5, block='b', rate=rate)
+    x = identity_block_lite(x, 3, [512, 512, 2048], stage=5, block='c', rate=rate)
+
+    x = AveragePooling2D((7, 7), name='avg_pool')(x)
+
+    if include_top:
+        x = Flatten()(x)
+        x = Dense(classes, activation='softmax', name='fc1000')(x)
+    else:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = GlobalMaxPooling2D()(x)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+    # Create model.
+    model = Model(inputs, x, name='resnet50')
+
+    return model
+
 
 if __name__ == "__main__":
     os.environ['CUDA_VISIBLE_DEVICES'] = ''
--- my_models/vgg16_modified.py
+++ my_models/vgg16_modified.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 from keras.models import Model
-from keras.layers import Flatten, Dense, Input, MaxPooling2D, GlobalAveragePooling2D, GlobalMaxPooling2D
+from keras.layers import Flatten, Dense, Input, MaxPooling2D, GlobalAveragePooling2D, GlobalMaxPooling2D, Conv2D
 from keras.engine.topology import get_source_inputs
 from keras import backend as K
 from keras.applications.imagenet_utils import _obtain_input_shape
@@ -88,5 +88,80 @@ def ModifiedVGG16(include_top=True, weights='imagenet',
     # Create model.
     model = Model(inputs, x, name='vgg16')
 
+
+    return model
+
+def VGG16_lite(include_top=True, weights='imagenet',
+          input_tensor=None, input_shape=None,
+          pooling=None,
+          classes=1000,
+               rate=4):
+
+    # Determine proper input shape
+    input_shape = _obtain_input_shape(input_shape,
+                                      default_size=224,
+                                      min_size=48,
+                                      data_format=K.image_data_format(),
+                                      require_flatten=include_top,
+                                      weights=weights)
+
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+    import my_models
+
+    # Block 1
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(64/rate)), (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(64/rate)), (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(128/rate)), (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(128/rate)), (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(256/rate)), (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(256/rate)), (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(256/rate)), (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(512/rate)), (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(512/rate)), (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(512/rate)), (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(512/rate)), (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(512/rate)), (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(max(my_models.LEAST_ATOMS, int(512/rate)), (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    if include_top:
+        # Classification block
+        x = Flatten(name='flatten')(x)
+        x = Dense(4096, activation='relu', name='fc1')(x)
+        x = Dense(4096, activation='relu', name='fc2')(x)
+        x = Dense(classes, activation='softmax', name='predictions')(x)
+    else:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = GlobalMaxPooling2D()(x)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+    # Create model.
+    model = Model(inputs, x, name='vgg16')
 
     return model
